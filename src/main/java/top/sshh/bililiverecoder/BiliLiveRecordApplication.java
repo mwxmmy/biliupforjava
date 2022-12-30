bf25aea48688f83aa0440b26fb360947367e3d30
mwxmmy <admin@sshh.top>
2022-12-30 19:52:59 +0800

    恢复定时任务

--- src/main/java/top/sshh/bililiverecoder/BiliLiveRecordApplication.java
+++ src/main/java/top/sshh/bililiverecoder/BiliLiveRecordApplication.java
@@ -3,9 +3,10 @@ package top.sshh.bililiverecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
-//@EnableScheduling
+@EnableScheduling
 @SpringBootApplication
 public class BiliLiveRecordApplication {
 

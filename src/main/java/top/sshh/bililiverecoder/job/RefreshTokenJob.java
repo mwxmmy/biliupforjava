c9b4ab80ff89e64dd0de47c68de7278961b0a5b8
mwxmmy <admin@sshh.top>
2023-04-26 11:10:50 +0800

    fix:修复刷新token任务失效

--- src/main/java/top/sshh/bililiverecoder/job/RefreshTokenJob.java
+++ src/main/java/top/sshh/bililiverecoder/job/RefreshTokenJob.java
@@ -3,6 +3,7 @@ package top.sshh.bililiverecoder.job;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import top.sshh.bililiverecoder.entity.BiliBiliUser;
 import top.sshh.bililiverecoder.repo.BiliUserRepository;
@@ -19,7 +20,7 @@ public class RefreshTokenJob {
     private BiliBiliUserService userService;
 
 
-    // @Scheduled(fixedDelay = 72000000, initialDelay = 60000)
+    @Scheduled(fixedDelay = 72000000, initialDelay = 60000)
     public void sndMsgProcess() {
         Iterable<BiliBiliUser> all = userRepository.findAll();
         for (BiliBiliUser user : all) {

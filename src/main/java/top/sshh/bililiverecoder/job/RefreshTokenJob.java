50533aa322fff222861f40089ea9c1dd564974ef
mwxmmy <admin@sshh.top>
2023-04-26 10:30:31 +0800

    feat:新增导出配置功能

--- src/main/java/top/sshh/bililiverecoder/job/RefreshTokenJob.java
+++ src/main/java/top/sshh/bililiverecoder/job/RefreshTokenJob.java
@@ -3,7 +3,6 @@ package top.sshh.bililiverecoder.job;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import top.sshh.bililiverecoder.entity.BiliBiliUser;
 import top.sshh.bililiverecoder.repo.BiliUserRepository;
@@ -20,7 +19,7 @@ public class RefreshTokenJob {
     private BiliBiliUserService userService;
 
 
-    @Scheduled(fixedDelay = 72000000, initialDelay = 60000)
+    // @Scheduled(fixedDelay = 72000000, initialDelay = 60000)
     public void sndMsgProcess() {
         Iterable<BiliBiliUser> all = userRepository.findAll();
         for (BiliBiliUser user : all) {
@@ -28,6 +27,7 @@ public class RefreshTokenJob {
                 userService.refreshToken(user);
             }catch (Exception e){
                 log.error("刷新token失败==>{}", JSON.toJSONString(user));
+                e.printStackTrace();
             }
         }
     }
